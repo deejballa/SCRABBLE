4f31464f358e58078de8a6eddc416aafd6607db7
dj frandsen <david.frandsen@eesoh.com>
2016-04-08 19:05:58 -0600

    fixed bug in displaying results and began to clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
 
   root 'home#index'
-  get '/about' => 'home#about'
-
-  get '/result' => 'home#temp'
+  get '/result' => 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
